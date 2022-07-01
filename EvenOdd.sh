f498a623033eeecab2ddd69b024f9a6bf1428868
SushreeSurelee <102430491+SushreeSurelee@users.noreply.github.com>
2022-07-01 10:09:48 +0530

    made changes on If Else

--- EvenOdd.sh
+++ EvenOdd.sh
@@ -3,9 +3,9 @@
 declare -a numbers
 numbers=(2 4 5 6 8 9 7 1)
 
-for ((i=0;i<=${#numbers[@]};i++))
+for ((i=0;i<=7;i++))
 do
-	if [ ((${numbers[0]}%2)) -eq 0 ]
+	if [ $((numbers%2)) -eq 0 ]
 	then
 		echo "The number is Even";
 	else
