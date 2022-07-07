983d1271292fe91a9109309bebdf5848cb2e7d1f
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-07 09:45:24 +0530

    [Sushree Modify Introduced new Array]

--- EvenOdd.sh
+++ EvenOdd.sh
@@ -3,10 +3,14 @@
 declare -a numbers
 numbers=(2 4 5 6 8 9 7 1)
 
+declare -a even
+declare -a odd
+even=0
+odd=0
 
 for ((i=0;i<${#numbers[@]};i++))
 do
-	if [ $((numbers%2)) -eq 0 ]
+	if [ $((numbers[@]%2)) -eq 0 ]
 	then
 		echo "The number is Even";
 	else
